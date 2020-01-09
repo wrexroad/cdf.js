9ada49aa2f80be931d34117d9202e0fa74a21338
Warren Rexroad <wrexroad@gmail.com>
2020-01-08 18:21:40 -0800

    started creating record objectsx

--- CDF.js
+++ CDF.js
@@ -1,3 +1,12 @@
+const COPYRIGHT_TEXT = `
+Common Data Format (CDF)
+(C) Copyright 1990-2015 NASA/GSFC
+Space Physics Data Facility
+NASA/Goddard Space Flight Center
+Greenbelt, Maryland 20771 USA
+(Internet -- GSFC-CDF-SUPPORT@LISTS.NASA.GOV)
+`;
+
 function CDF() {
   this.magic = [0xCDF30001, 0x0000FFFF];
 
@@ -26,95 +35,258 @@ CDF.prototype.setCompression = function(isCompressed, method) {
   }
 }
 
-function Record(t) {
-  if (!Record.TYPES[t]) {
-    return null;
-  }
+function Record() {
+  this.fields = new Map();
+  return this;
+}
+Record.prototype.type = 0;
+Record.prototype.size = 0;
+Record.prototype.toBytes = function() {
+  let
+    buf = Buffer.alloc(this.size),
+    view = DataView(buf.buffer);
 
-  let fields = FieldCollections[t]();
+  console.log(buf, view);
+}
 
-  Object.defineProperties(this, {
-    type: {
-      get: () => Record.TYPES[t]
-    },
-    fields: {
-      get: () => fields
-    }
-  });
+function CDR() {
+  Record.call(this);
+
+  this.fields.set("GDRoffset", {size: this.size, type: "BigInt64", value: 312}),
+  this.fields.set("Version", {size: this.size, type: "BigInt64", value: 312}),
+  this.fields.set("Release", {size: this.size, type: "BigInt64", value: 312}),
+  this.fields.set("Encoding", {size: this.size, type: "BigInt64", value: 312}),
+  this.fields.set("Flags", {size: this.size, type: "BigInt64", value: 312}),
+  this.fields.set("rfuA", {size: this.size, type: "BigInt64", value: 0}),
+  this.fields.set("rfuB", {size: this.size, type: "BigInt64", value: 0}),
+  this.fields.set("Increment", {size: this.size, type: "BigInt64", value: 312}),
+  this.fields.set("Identifier", {size: this.size, type: "BigInt64", value: 312}),
+  this.fields.set("rfuE", {size: this.size, type: "BigInt64", value: -1}),
+  this.fields.set("Copyright", {size: 256, type: "ascii", value: COPYRIGHT_TEXT})
+  ];
 
   return this;
-} 
+}
+CDR.prototype = Object.create(Record.prototype);
+CDR.prototype.constructor = CDR;
+CDR.prototype.type = 1;
+CDR.prototype.getSize = function() {return 312;}
+
+function GDR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {get: () => 312}
+  });
+  return this;
+}
+GDR.prototype = Object.create(Record.prototype);
+GDR.prototype.constructor = GDR;
+GDR.prototype.type = 2;
+
+function RVDR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+RVDR.prototype = Object.create(Record.prototype);
+RVDR.prototype.constructor = RVDR;
+RVDR.prototype.type = 3;
+
+function ADR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+ADR.prototype = Object.create(Record.prototype);
+ADR.prototype.constructor = ADR;
+ADR.prototype.type = 4;
+
+function AGR_EDR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+AGR_EDR.prototype = Object.create(Record.prototype);
+AGR_EDR.prototype.constructor = AGR_EDR;
+AGR_EDR.prototype.type = 5;
+
+function VXR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+VXR.prototype = Object.create(Record.prototype);
+VXR.prototype.constructor = VXR;
+VXR.prototype.type = 6;
+
+function VVR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+VVR.prototype = Object.create(Record.prototype);
+VVR.prototype.constructor = VVR;
+VVR.prototype.type = 7;
+
+function ZVDR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+ZVDR.prototype = Object.create(Record.prototype);
+ZVDR.prototype.constructor = ZVDR;
+ZVDR.prototype.type = 8;
+
+function AZ_EDR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+AZ_EDR.prototype = Object.create(Record.prototype);
+AZ_EDR.prototype.constructor = AZ_EDR;
+AZ_EDR.prototype.type = 9;
+
+function CCR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+CCR.prototype = Object.create(Record.prototype);
+CCR.prototype.constructor = CCR;
+CCR.prototype.type = 10;
+
+function CPR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+CPR.prototype = Object.create(Record.prototype);
+CPR.prototype.constructor = CPR;
+CPR.prototype.type = 11;
+
+function SPR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+SPR.prototype = Object.create(Record.prototype);
+SPR.prototype.constructor = SPR;
+SPR.prototype.type = 12;
+
+function CVVR() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+CVVR.prototype = Object.create(Record.prototype);
+CVVR.prototype.constructor = CVVR;
+CVVR.prototype.type = 13;
+
+function URI() {
+  Record.call(this);
+  
+  Object.defineProperties(this, {
+    size: {
+      get: 
+    }
+  });
+  return this;
+}
+URI.prototype = Object.create(Record.prototype);
+URI.prototype.constructor = URI;
+URI.prototype.type = -1;
+
+
+function Field(name, type, value) {
+  this.name = name;
+  this.type = type;
+  this.value = value;
+
+
+
+  return this;
+}
+Field.prototype.toBytes = function() {
+  return 
+}
 
 FieldCollections = {
-  CDR: function() {
-    return [
-      ["GDRoffset", "s8", 0],
-      ["Version", "s4", 0],
-      ["Release", "s4", 0],
-      ["Encoding", "s4", 0],
-      ["Flags", "s4", 0],
-      ["rfuA", "s4", 0],
-      ["rfuB", "s4", 0],
-      ["Increment", "s4", 0],
-      ["Identifier", "s4", 3],
-      ["rfuE", "s4", -1],
-      ["Copyright", "ascii", [""], 256]
-    ]
-  },
+  
   GDR: function() {
     return [
-      ["rVDRhead", "s8", 0],
-      ["zVDRhead", "s8", 0],
-      ["ADRhead", "s8", 0],
-      ["eof", "s8", 0],
-      ["NrVars", "s4", 0],
-      ["NumAttr", "s4", 0],
-      ["rMaxRec", "s4", 0],
-      ["rNumDims", "s4", 0],
-      ["NzVars", "s4", 0],
-      ["UIRhead", "s8", 0],
-      ["rfuC", "s4", 0],
-      ["LeapSecondLastUpdated", "s4", 0],
-      ["rfuE", "s4", 0],
-      ["rDimSizes", "s4", [], 0]
+      ["rVDRhead", "BigInt64", 0],
+      ["zVDRhead", "BigInt64", 0],
+      ["ADRhead", "BigInt64", 0],
+      ["eof", "BigInt64", 0],
+      ["NrVars", "Int32", 0],
+      ["NumAttr", "Int32", 0],
+      ["rMaxRec", "Int32", 0],
+      ["rNumDims", "Int32", 0],
+      ["NzVars", "Int32", 0],
+      ["UIRhead", "BigInt64", 0],
+      ["rfuC", "Int32", 0],
+      ["LeapSecondLastUpdated", "Int32", 0],
+      ["rfuE", "Int32", 0],
+      ["rDimSizes", "Int32", [], 0]
     ];
   },
-  RVDR: function() {
-    return this;
-  },
-  ADR: function() {
-    return this;
-  },
-  AGR_EDR: function() {
-    return this;
-  },
-  VXR: function() {
-    return this;
-  },
-  VVR: function() {
-    return this;
-  },
-  ZVDR: function() {
-    return this;
-  },
-  AZ_EDR: function() {
-    return this;
-  },
-  CCR: function() {
-    return this;
-  },
-  CPR: function() {
-    return this;
-  },
-  SPR: function() {
-    return this;
-  },
-  CVVR: function() {
-    return this;
-  },
-  URI: function() {
-    return this;
-  }
+  
 }
 Record.TYPES = {
   CDR: 1,
