0edc3776b933906c723b2a4d7e786748b967b6c4
Warren Rexroad <wrexroad@gmail.com>
2020-01-13 19:39:24 -0800

    added more records

--- CDF.js
+++ CDF.js
@@ -80,6 +80,84 @@ CDF.prototype.getTotalSize = function() {
   return this.records.recude((size, rec) => (size += rec.getSize()), 0);
 }
 
+CDF.DATA_TYPES = {
+  1: {id: 1, name: "CDF_INT1", size: 1, typedArray: Int8Array},
+  2: {id: 2, name: "CDF_INT2", size: 2, typedArray: Int16Array},
+  4: {id: 4, name: "CDF_INT4", size: 4, typedArray: Int32Array},
+  8: {id: 8, name: "CDF_INT8", size: 8, typedArray: BigInt64Array},
+  11: {id: 11, name: "CDF_UINT1", size: 1, typedArray: Uint8Array},
+  12: {id: 12, name: "CDF_UINT2", size: 2, typedArray: Uint16Array},
+  14: {id: 14, name: "CDF_UINT4", size: 4, typedArray: Uint32Array},
+  41: {id: 41, name: "CDF_BYTE", size: 4, typedArray: Int8Array},
+  21: {id: 21, name: "CDF_REAL4", size: 4, typedArray: Float32Array},
+  22: {id: 22, name: "CDF_REAL8", size: 8, typedArray: Float64Array},
+  44: {id: 44, name: "CDF_FLOAT", size: 4, typedArray: Float32Array},
+  45: {id: 45, name: "CDF_DOUBLE", size: 8, typedArray: Float64Array},
+  31: {id: 31, name: "CDF_EPOCH", size: 8, typedArray: Float64Array},
+  32: {id: 32, name: "CDF_EPOCH16", size: 16, typedArray: Float64Array},//not supported yet
+  33: {id: 33, name: "CDF_TIME_TT2000", size: 8, typedArray: BigInt64Array},
+  51: {id: 51, name: "CDF_CHAR", size: 1, typedArray: Uint8Array},
+  52: {id: 52, name: "CDF_UCHAR", size: 1, typedArray: Uint8Array}
+};
+CDF.DATA_TYPES.CDF_INT1 = CDF.DATA_TYPES[1];
+CDF.DATA_TYPES.CDF_INT2 = CDF.DATA_TYPES[2];
+CDF.DATA_TYPES.CDF_INT4 = CDF.DATA_TYPES[4];
+CDF.DATA_TYPES.CDF_INT8 = CDF.DATA_TYPES[8];
+CDF.DATA_TYPES.CDF_UINT1 = CDF.DATA_TYPES[11];
+CDF.DATA_TYPES.CDF_UINT2 = CDF.DATA_TYPES[12];
+CDF.DATA_TYPES.CDF_UINT4 = CDF.DATA_TYPES[14];
+CDF.DATA_TYPES.CDF_BYTE = CDF.DATA_TYPES[41];
+CDF.DATA_TYPES.CDF_REAL4 = CDF.DATA_TYPES[21];
+CDF.DATA_TYPES.CDF_REAL8 = CDF.DATA_TYPES[22];
+CDF.DATA_TYPES.CDF_FLOAT = CDF.DATA_TYPES[44];
+CDF.DATA_TYPES.CDF_DOUBLE = CDF.DATA_TYPES[45];
+CDF.DATA_TYPES.CDF_EPOCH = CDF.DATA_TYPES[31];
+CDF.DATA_TYPES.CDF_EPOCH16 = CDF.DATA_TYPES[32];
+CDF.DATA_TYPES.CDF_TIME_TT2000 = CDF.DATA_TYPES[33];
+CDF.DATA_TYPES.CDF_CHAR = CDF.DATA_TYPES[51];
+CDF.DATA_TYPES.CDF_UCHAR = CDF.DATA_TYPES[52];
+
+CDF.ENCODINGS = {
+  1: {id: 1, name: "NETWORK_ENCODING"},
+  2: {id: 1, name: "SUN_ENCODING"},
+  3: {id: 1, name: "VAX_ENCODING", littleEndian: true},
+  4: {id: 1, name: "DECSTATION_ENCODING", littleEndian: true},
+  5: {id: 1, name: "SGi_ENCODING"},
+  6: {id: 1, name: "IBMPC_ENCODING", littleEndian: true},
+  7: {id: 1, name: "IBMRS_ENCODING"},
+  9: {id: 1, name: "PPC_ENCODING"},
+  11: {id: 1, name: "HP_ENCODING", littleEndian: true},
+  12: {id: 1, name: "NeXT_ENCODING"},
+  13: {id: 1, name: "ALPHAOSF1_ENCODING", littleEndian: true},
+  14: {id: 1, name: "ALPHAVMSd_ENCODING", littleEndian: true},
+  15: {id: 1, name: "ALPHAVMSg_ENCODING", littleEndian: true},
+  16: {id: 1, name: "ALPHAVMSi_ENCODING", littleEndian: true},
+  17: {id: 1, name: "ARM_LITTLE_ENCODING", littleEndian: true},
+  18: {id: 1, name: "ARM_BIG_ENCODING"},
+  19: {id: 1, name: "iA64VMSi_ENCODING", littleEndian: true},
+  20: {id: 1, name: "iA64VMSs_ENCODING", littleEndian: true},
+  21: {id: 1, name: "iA64VMSg_ENCODING", littleEndian: true}
+}
+CDF.ENCODINGS.NETWORK_ENCODING = CDF.ENCODINGS[1];
+CDF.ENCODINGS.SUN_ENCODING = CDF.ENCODINGS[2];
+CDF.ENCODINGS.VAX_ENCODING = CDF.ENCODINGS[3];
+CDF.ENCODINGS.DECSTATION_ENCODING = CDF.ENCODINGS[4];
+CDF.ENCODINGS.SGi_ENCODING = CDF.ENCODINGS[5];
+CDF.ENCODINGS.IBMPC_ENCODING = CDF.ENCODINGS[6];
+CDF.ENCODINGS.IBMRS_ENCODING = CDF.ENCODINGS[7];
+CDF.ENCODINGS.PPC_ENCODING = CDF.ENCODINGS[9];
+CDF.ENCODINGS.HP_ENCODING = CDF.ENCODINGS[11];
+CDF.ENCODINGS.NeXT_ENCODING = CDF.ENCODINGS[12];
+CDF.ENCODINGS.ALPHAOSF1_ENCODING = CDF.ENCODINGS[13];
+CDF.ENCODINGS.ALPHAVMSd_ENCODING = CDF.ENCODINGS[14];
+CDF.ENCODINGS.ALPHAVMSg_ENCODING = CDF.ENCODINGS[15];
+CDF.ENCODINGS.ALPHAVMSi_ENCODING = CDF.ENCODINGS[16];
+CDF.ENCODINGS.ARM_LITTLE_ENCODING = CDF.ENCODINGS[17];
+CDF.ENCODINGS.ARM_BIG_ENCODING = CDF.ENCODINGS[18];
+CDF.ENCODINGS.iA64VMSi_ENCODING = CDF.ENCODINGS[19];
+CDF.ENCODINGS.iA64VMSs_ENCODING = CDF.ENCODINGS[20];
+CDF.ENCODINGS.iA64VMSg_ENCODING = CDF.ENCODINGS[21];
+
 function Record(args) {
   this.id = Math.random().toString(36).split(".")[1];
   this.type = args.callee.name;
@@ -90,11 +168,14 @@ function Record(args) {
   
   return this;
 }
+
 Record.prototype.getSize = function(){
-  let size = 0;
+  let size = 8; //start with 8 because we will skip the RecordSize field to prevent loop
   for (let name in this.fields) {
+    if (name === "RecordSize") {return;}
+
     let
-      field = this.fields[name],
+      field = this.fields.get(name),
       type = field.type,
       value = field.value,
       numEls = field.fixedWidth || value.length;
@@ -124,6 +205,17 @@ Record.prototype.updateField = function(name, value) {
   this.fields.set(name, field);
   return this;
 }
+Record.prototype.hasFlag = function(flg) {
+  return this.fields.get("Flags").value & flg;
+}
+Record.prototype.setFlag = function(flg) {
+  let flags = this.fields.get("Flags");
+  if (!flags) {return false;}
+  flags.value |= flg;
+  this.fields.set("Flags", flags);
+
+  return true;
+}
 Record.prototype.toBytes = function() {
   let
     buf = Buffer.alloc(this.getSize()),
@@ -132,7 +224,7 @@ Record.prototype.toBytes = function() {
 
   for (let name in this.fields) {
     let
-      field = this.fields[name],
+      field = this.fields.get(name),
       type = field.type,
       value = field.value,
       numEls = field.fixedWidth || value.length;
@@ -227,34 +319,86 @@ GDR.prototype.getSize = function() {
  return 84 + this.fields("rNumDims").value;
 };
 
-function RVDR(cdf) {
-  Record.call(this, arguments);
-  
+function VDR() {
   this.addField("VDRnext", DATA_TYPES.BigInt64, null);
   this.addField("DataType", DATA_TYPES.Int32, null);
-  this.addField("MaxRec", DATA_TYPES.Int32, null);
+  this.addField("MaxRec", DATA_TYPES.Int32, -1);
   this.addField("VXRhead", DATA_TYPES.BigInt64, null);
   this.addField("VXRtail", DATA_TYPES.BigInt64, null);
-  this.addField("Flags", DATA_TYPES.Int32, null);
+  this.addField("Flags", DATA_TYPES.Int32, 0);
   this.addField("SRecrods", DATA_TYPES.Int32, null);
   this.addField("rfuB", DATA_TYPES.Int32, 0);
   this.addField("rfuC", DATA_TYPES.Int32, -1);
   this.addField("rfuF", DATA_TYPES.Int32, -1);
-  this.addField("NumElems", DATA_TYPES.Int32, -1);
-  this.addFIeld("CPRorSPRoffset", DATA_TYPES.BigInt64, null);
-  this.addField("NumElems", DATA_TYPES.Int32, -1);
-  this.addField("BlockingFactor", DATA_TYPES.Int32, null);// 4 bytes Offset:80
-  this.addField("Name", DATA_TYPES.ascii, "", 256);// 256 bytes Offset:84. Was 64 bytes in earlier V2.*
-  this.addField("zNumDims", DATA_TYPES.Int32, null);// 4 bytes Offset:340 if a zVDR. Not present if an rVDR.
-  this.addField("zDimSizes", DATA_TYPES.Int32, null);
-  this.addField("DimVarys", DATA_TYPES.Int16, null);// 4 bytes Size depends on the zNumDims field if a zVDR (but not present if zero
-  this.addField("PadValue", DATA_TYPES.Int16, null);// Variable Size depends on DataType and NumElems fields. Not present if bit 1 of
-  this.addField("Flags", DATA_TYPES.Int16, null);// field is not set.
+  this.addField("NumElems", DATA_TYPES.Int32, 1);
+  this.addField("Num", DATA_TYPES.Int32, 1);
+  this.addField("CPRorSPRoffset", DATA_TYPES.BigInt64, null);
+  this.addField("BlockingFactor", DATA_TYPES.Int32, null);
+  this.addField("Name", DATA_TYPES.ascii, "", 256);
+  if (this.type == "ZVDR") {
+    this.addField("zNumDims", DATA_TYPES.Int32, null);
+    this.addField("zDimSizes", DATA_TYPES.Int32, null);  
+  }
+  this.addField("DimVarys", DATA_TYPES.Int32, 0);
   
   return this;
 }
+VDR.prototype.setDataType = function(dt, numElems, isPadded) {
+  this.updateField("DataType", CDF.DATA_TYPES[dt].id);
+  this.updateField("NumElems", numElems || 1);
+  if (isPadded) {
+    this.setFlag(VDR.FLAGS.PAD);
+  }
+}
+VDR.prototype.getSize = function() {
+  return (
+    (this.type == "ZVDR"? 352 : 344) +
+    (this.hasFlag(VDR.FLAGS.PAD) ? this.getPadBytes().length : 0)
+  );
+};
+VDR.prototype.getPadBytes = function() {
+  let hasPad = this.getFlag(VDR.FLAGS.PAD);
+
+  if (!hasPad) {return Buffer.alloc(0)}
+
+  let
+    len = this.fields.get("NumElems").value,
+    dt = CDF.DATA_TYPES[this.fields.get("DataType").value],
+    pad = (new dt.typedArray(len)).fill(dt.pad);
+
+  return pad.buffer;
+}
+VDR.prototype.toBytes = function() {
+  let
+    recBuf = Record.prototype.toBytes.call(this),
+    padBuf = this.getPadBytes();
+  return Buffer.concat([recBuf, padBuf], recBuf.length + padBuf.length);
+}
+VDR.FLAGS = {
+  VARIANCE: 0b001,
+  PAD: 0b010,
+  COMPRESSION: 0b100
+}
+
+function RVDR(cdf) {
+  Record.call(this, arguments);
+  VDR.call(this, arguments);
+
+  return this;
+}
 RVDR.prototype = Object.create(Record.prototype);
+Object.assign(RVDR.prototype, VDR.prototype);
 RVDR.prototype.constructor = RVDR;
+
+function ZVDR(cdf) {
+  Record.call(this, arguments);
+  VDR.call(this, arguments);
+
+  return this;
+}
+ZVDR.prototype = Object.create(Record.prototype);
+Object.assign(ZVDR.prototype, VDR.prototype);
+ZVDR.prototype.constructor = ZVDR;
 
 function ADR(cdf) {
   Record.call(this, arguments);
@@ -278,18 +422,62 @@ ADR.prototype = Object.create(Record.prototype);
 ADR.prototype.constructor = ADR;
 ADR.prototype.getSize = function() {return 324;}
 
+function AEDR(dt, val) {
+  if (!val.length) {val = [val]}
+
+  this.addField("AEDRnext", DATA_TYPES.BigInt64, null);
+  this.addField("AttrNum", DATA_TYPES.Int32, 0);
+  this.addField("DataType", DATA_TYPES.Int32, CDF.DATA_TYPES[dt].id);
+  this.addField("Num", DATA_TYPES.Int32, 0);
+  this.addField("NumElems", DATA_TYPES.Int32, val.length);
+  this.addField("NumStrings", DATA_TYPES.Int32,
+    typeof val === "string"? split("\\n").length : 1
+  );
+  this.addField("rfuB", DATA_TYPES.Int32, 0);
+  this.addField("rfuC", DATA_TYPES.Int32, 0);
+  this.addField("rfuD", DATA_TYPES.Int32, -1);
+  this.addField("rfuE", DATA_TYPES.Int32, -1);
+  this.addField("Value", dt/*FIX THIS*/, val);
+}
+AEDR.prototype.getValueBytes = function() {
+  let
+    len = this.fields.get("NumElems").value,
+    dt = CDF.DATA_TYPES[this.fields.get("DataType").value],
+    value = dt.typedArray.from(this.fields.get("Value").value)
+ 
+  return value.buffer;
+}
+AEDR.prototype.getSize = function() {
+  return 56 + (
+    this.fields.get("NumElems").value *
+    CDF.DATA_TYPES[this.fields.get("DataType").value].size
+  )
+}
+AEDR.prototype.toBytes = function() {
+  let 
+    rec = Record.prototype.toBytes.call(this),
+    val = this.getValueBytes();
+  return Buffer.concat([rec, val], rec.length + val.length)
+}
+
 function AGR_EDR(cdf) {
   Record.call(this, arguments);
-  
-  Object.defineProperties(this, {
-    size: {
-      get: 
-    }
-  });
+  AEDR.call(this, arguments);
   return this;
 }
 AGR_EDR.prototype = Object.create(Record.prototype);
+Object.assign(AGR_EDR.prototype, AEDR.prototype);
 AGR_EDR.prototype.constructor = AGR_EDR;
+
+function AZ_EDR(cdf) {
+  Record.call(this, arguments);
+  AEDR.call(this, arguments);
+  return this;
+}
+AGR_EDR.prototype = Object.create(Record.prototype);
+Object.assign(AGR_EDR.prototype, AEDR.prototype);
+AGR_EDR.prototype.constructor = AGR_EDR;
+
 
 function VXR(cdf) {
   Record.call(this, arguments);
@@ -339,44 +527,15 @@ VXR.prototype.addEntry = function(rec) {
 }
 VXR.N_ENTRIES = 7;
 
-function VVR(cdf) {
+function VVR(cdf, type, data) {
   Record.call(this, arguments);
+
+  this.addField("Records", type, data);
   
-  Object.defineProperties(this, {
-    size: {
-      get: 
-    }
-  });
   return this;
 }
 VVR.prototype = Object.create(Record.prototype);
 VVR.prototype.constructor = VVR;
-
-function ZVDR(cdf) {
-  Record.call(this, arguments);
-  
-  Object.defineProperties(this, {
-    size: {
-      get: 
-    }
-  });
-  return this;
-}
-ZVDR.prototype = Object.create(Record.prototype);
-ZVDR.prototype.constructor = ZVDR;
-
-function AZ_EDR(cdf) {
-  Record.call(this, arguments);
-  
-  Object.defineProperties(this, {
-    size: {
-      get: 
-    }
-  });
-  return this;
-}
-AZ_EDR.prototype = Object.create(Record.prototype);
-AZ_EDR.prototype.constructor = AZ_EDR;
 
 function CCR(cdf) {
   Record.call(this, arguments);
